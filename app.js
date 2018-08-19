ab367bc4ac4ecdf5bb7ff2485e47fe54fef9d43e
Bex <bexicles@gmail.com>
2018-08-19 19:01:39 +0100

    rolled back change

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.get('/auth/google/callback', (req, res, next) => {
         // console.log("Given name: "+ givenName);
 
         console.log("Google callback called, redirecting to dashboard"+ req.session.token);
-        res.render('dashboard', {name: req.session.passport.user.name, bex_monzo: '52.06', peet_monzo: '66.43', bex_firstdirect: '150.23', peet_lloyds: '9,998.12', bex_barclaycard: '-500', peet_mbna1: '-9,786.99'});
+        res.render('dashboard', {name: "Bex", bex_monzo: '52.06', peet_monzo: '66.43', bex_firstdirect: '150.23', peet_lloyds: '9,998.12', bex_barclaycard: '-500', peet_mbna1: '-9,786.99'});
     }
 );
 
